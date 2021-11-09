972deea7c01bb631ee8eda33bde5bd729cce45a0
kushalgujrati <81164007+kushalgujrati@users.noreply.github.com>
2021-11-10 01:36:40 +0530

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,13 @@
+total = 0
+
+list = [10, 9, 7, 8]
+
+for ele in range(0, len(list)):
+    total = total + list[ele]
+ 
+print("Sum of all elements in given list: ", total)
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
